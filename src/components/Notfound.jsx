8e4ae1bac2b562e7976113a6d5ef7f398d73306b
meitavlapid <meitavlapid@gmail.com>
2025-01-28 14:31:51 +0200

    121212

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -7,14 +7,12 @@ function Notfound() {
     <>
       <div className="container text-center">
         <h1 className="notfound m-5">
-          {" "}
           <i className="fa-solid fa-exclamation fa-bounce fa-xl text-danger "></i>
           404 Not Found
           <i className="fa-solid fa-exclamation fa-bounce fa-xl text-danger"></i>
         </h1>
       </div>
       <button className="btn btn-warning m-5 w-25" onClick={() => nav(-1)}>
-        {" "}
         GO BACK
       </button>
     </>
