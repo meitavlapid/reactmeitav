6562202f57ef4df5e0000ad1f7fb303f95f550bb
meitavlapid <meitavlapid@gmail.com>
2025-01-28 15:26:44 +0200

    11

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -5,18 +5,7 @@ function NotFound() {
   // let nav = useNavigate();
   return (
     <>
-      <div className="container text-center">
-        <h1 className="notfound m-5">not found</h1>
-      </div>
-      {/* <h1 className="notfound m-5">
-          <i className="fa-solid fa-exclamation fa-bounce fa-xl text-danger "></i>
-          404 Not Found
-          <i className="fa-solid fa-exclamation fa-bounce fa-xl text-danger"></i>
-        </h1>
-      </div>
-      <button className="btn btn-warning m-5 w-25" onClick={() => nav(-1)}>
-        GO BACK
-      </button> */}
+      <div>Page Not Found</div>;
     </>
   );
 }
